491cc9d4594917a49c66cf0e65a9f0eadda1a62e
lyricalnovaa <lyricalnovaprod@gmail.com>
2025-09-22 12:57:51 -0500

    Update key.js

--- server/models/key.js
+++ server/models/key.js
@@ -1,1 +1,15 @@
+const rolePermissionMapping = {
+    'Police Officer': 'police.update_status,police.lookup_plate',
+    'Police Supervisor': 'police.supervisor,police.update_status,police.lookup_plate,admin.manage_units',
+    'Dispatch': 'dispatch.create_call,dispatch.assign_unit',
+    'Dispatch Supervisor': 'dispatch.supervisor,dispatch.create_call,dispatch.assign_unit,admin.manage_calls',
+    'Fire/EMS': 'ems.respond_call,ems.add_patient_notes',
+    'Fire Captain': 'fire.captain,ems.respond_call,ems.add_patient_notes,admin.manage_units',
+    'Civilian': 'civilian.create_character',
+    'DOT': 'dot.control_traffic',
+    'Admin': 'admin.full_access'
+};
 
+module.exports = {
+    rolePermissionMapping
+};
