6d088c627ff616ce12047060df31fe8d94865a86
lyricalnovaa <lyricalnovaprod@gmail.com>
2025-09-22 13:05:05 -0500

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,8 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    if (token) {
+        localStorage.setItem('token', token);
+        window.location.href = '/dashboard.html';
+    }
+});
