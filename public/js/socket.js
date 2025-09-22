f1e009b500ba45167eda56e0bcaf935cd9047cd0
lyricalnovaa <lyricalnovaprod@gmail.com>
2025-09-22 13:08:49 -0500

    Update socket.js

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,1 +1,18 @@
+const socket = io();
 
+// Event listeners for incoming real-time data
+socket.on('live_calls_update', (data) => {
+    // This event handler will be managed by the dashboard.js file
+    const event = new CustomEvent('callsUpdate', { detail: data });
+    window.dispatchEvent(event);
+});
+
+socket.on('live_units_update', (data) => {
+    // This event handler will be managed by the dashboard.js file
+    const event = new CustomEvent('unitsUpdate', { detail: data });
+    window.dispatchEvent(event);
+});
+
+// You can add more listeners for panic buttons, chat messages, etc.
+
+export { socket };
