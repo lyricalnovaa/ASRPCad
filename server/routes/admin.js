c2cf8266f6e42af33a17bebc2d7ed53c9c5a5ad5
lyricalnovaa <lyricalnovaprod@gmail.com>
2025-09-22 12:59:57 -0500

    Update admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const adminRouter = express.Router();
+const { getAllUsers, updateUser } = require('../models/user');
+const { authenticateJWT, checkPermission } = require('../middleware/auth');
 
+adminRouter.get('/users', authenticateJWT, checkPermission('admin.full_access'), async (req, res) => {
+    const users = await getAllUsers();
+    res.json(users);
+});
+
+adminRouter.post('/users/assign-key', authenticateJWT, checkPermission('admin.full_access'), async (req, res) => {
+    const { userId, permissionKey, role } = req.body;
+    await updateUser(userId, { permissionKey, role });
+    res.json({ message: `Permission key '${permissionKey}' assigned to user '${userId}'.` });
+});
+
+module.exports = adminRouter;
