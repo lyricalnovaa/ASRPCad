a74c94c92921e60b5d3d9cf75b8ff0b43e3b504c
lyricalnovaa <lyricalnovaprod@gmail.com>
2025-09-22 23:43:14 -0500

    Update firebase.js

--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -2,12 +2,12 @@ const admin = require('firebase-admin');
 require('dotenv').config();
 
 try {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+  const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
 } catch (error) {
-  console.error("Failed to parse FIREBASE_SERVICE_ACCOUNT_JSON. Ensure it is a valid stringified JSON object.", error);
+  console.error("Failed to parse GOOGLE_APPLICATION_CREDENTIALS. Ensure it is a valid stringified JSON object.", error);
   process.exit(1);
 }
 
